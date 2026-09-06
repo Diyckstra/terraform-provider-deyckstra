d3c3e7a73c19af59c78358262163c425aae507c3
Anton Kurbatov <deyckstra@gmail.com>
2026-09-06 12:59:24 +0300

    ec2: forbid specifying both instance and network_interface in aws_eip

--- internal/service/ec2/ec2_eip.go
+++ internal/service/ec2/ec2_eip.go
@@ -94,9 +94,10 @@ func ResourceEIP() *schema.Resource {
 				Computed: true,
 			},
 			"instance": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"network_interface"},
 			},
 			"network_border_group": {
 				Type:     schema.TypeString,
@@ -105,9 +106,10 @@ func ResourceEIP() *schema.Resource {
 				ForceNew: true,
 			},
 			"network_interface": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Type:          schema.TypeString,
+				Optional:      true,
+				Computed:      true,
+				ConflictsWith: []string{"instance"},
 			},
 			"private_dns": {
 				Type:     schema.TypeString,
